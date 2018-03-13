5573fbef6c6f685780cd909f87178788fcec6d8e
neil <2175191559@qq.com>
2018-03-13 18:44:25 +0800

    init project

---
+++ src/main/java/personal/weizeng/portals/App.java
@@ -0,0 +1,13 @@
+package personal.weizeng.portals;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
